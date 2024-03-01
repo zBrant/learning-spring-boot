d57c3ab6487d5a3fb5dd2061b95d0e8be33fba59
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-01 14:05:58 -0400

    update

--- vendas/src/main/java/io/github/zbrant/Animal.java
+++
@@ -1,6 +0,0 @@
-package io.github.zbrant;
-
-public interface Animal {
-  void fazerBarulho();
-
-}
---
+++ vendas/src/main/java/io/github/zbrant/domain/entity/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.zbrant.domain.entity;
+
+public class Cliente {
+
+  private Integer id;
+  private Integer nome;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getNome() {
+    return nome;
+  }
+
+  public void setNome(Integer nome) {
+    this.nome = nome;
+  }
+}
