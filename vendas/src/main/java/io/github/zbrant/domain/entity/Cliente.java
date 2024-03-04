11c1e91d9cd55ccb27ec92df3ba1e692cc02182a
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-04 13:57:24 -0400

    simple rest api created

--- vendas/src/main/java/io/github/zbrant/domain/entity/Cliente.java
+++ vendas/src/main/java/io/github/zbrant/domain/entity/Cliente.java
@@ -1,5 +1,7 @@
 package io.github.zbrant.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -14,6 +16,9 @@ public class Cliente {
   @Column(name = "nome", length = 100)
   private String nome;
 
+  @Column(name = "cpf", length = 11)
+  private String cpf;
+  @JsonIgnore
   @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
   private Set<Pedido> pedidos;
 
@@ -51,6 +56,14 @@ public class Cliente {
 
   public void setNome(String nome) {
     this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
   }
 
   @Override
