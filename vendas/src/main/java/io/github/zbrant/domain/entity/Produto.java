da87d78aa954f90ad04307e29299fd9b5ea0656f
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-06 08:27:13 -0400

    s5 Bean Validation

--- vendas/src/main/java/io/github/zbrant/domain/entity/Produto.java
+++ vendas/src/main/java/io/github/zbrant/domain/entity/Produto.java
@@ -20,10 +20,10 @@ public class Produto {
   private Integer id;
 
   @Column(name = "descricao")
-  @NotEmpty(message = "campo descricao é obrigatório")
+  @NotEmpty(message = "{campo.descricao.obrigatorio}")
   private String descricao;
 
   @Column(name = "preco_unitario")
-  @NotNull(message = "campo preço é obrigatório")
+  @NotNull(message = "{campo.preco.obrigatorio}")
   private BigDecimal preco;
 }
--- vendas/src/main/java/io/github/zbrant/rest/dto/PedidoDTO.java
+++ vendas/src/main/java/io/github/zbrant/rest/dto/PedidoDTO.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,13 +14,13 @@ import java.util.List;
 @AllArgsConstructor
 public class PedidoDTO {
 
-  @NotNull(message = "informe o código do cliente")
+  @NotNull(message = "{campo.codigo-cliente.obrigatorio}")
   private Integer cliente;
 
-  @NotNull(message = "campo total do pedido é obrigatório")
+  @NotNull(message = "{campo.total-pedido.obrigatorio}")
   private BigDecimal total;
 
-  @NotEmptyList(message = "Pedido não pode ser realizado sem itens")
+  @NotEmptyList(message = "{campo.items-pedido.obrigatorio}")
   private List<ItemPedidoDTO> items;
 
 }
