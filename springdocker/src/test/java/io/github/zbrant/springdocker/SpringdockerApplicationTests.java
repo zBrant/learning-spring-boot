d3dfa6d7729a5196d2b2e630c5649e79b830359e
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-08 14:01:39 -0400

    docker image created

---
+++ springdocker/src/test/java/io/github/zbrant/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
