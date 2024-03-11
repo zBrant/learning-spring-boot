66effc1c5422f596d9450998c9153cddedd8e0ec
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-11 10:40:49 -0400

    entities added

---
+++ sbootexp-security/src/main/java/io/github/zbrant/sbootexpsecurity/domain/entity/Grupo.java
@@ -0,0 +1,18 @@
+package io.github.zbrant.sbootexpsecurity.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Grupo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  private String nome;
+}
