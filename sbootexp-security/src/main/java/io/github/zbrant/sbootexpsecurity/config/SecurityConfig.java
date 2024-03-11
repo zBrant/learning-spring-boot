92eb04f3ff0001f7670d58c13e5a0ef169bdb8c3
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-11 10:08:10 -0400

    Custom authentication filter

--- sbootexp-security/src/main/java/io/github/zbrant/sbootexpsecurity/config/SecurityConfig.java
+++ sbootexp-security/src/main/java/io/github/zbrant/sbootexpsecurity/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,9 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(
-      HttpSecurity http, SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider) throws Exception{
+      HttpSecurity http,
+      SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+      CustomFilter customFilter) throws Exception{
     return http
         .authorizeHttpRequests( customizer -> {
           customizer.requestMatchers("/public").permitAll();
@@ -28,6 +31,7 @@ public class SecurityConfig {
         .httpBasic(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults())
         .authenticationProvider(senhaMasterAuthenticationProvider)
+        .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
