0a490a32d0131c4fd04a011332b0cc39da86d5a6
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-11 09:45:19 -0400

    simple authentication provider

--- sbootexp-security/src/main/java/io/github/zbrant/sbootexpsecurity/FooController.java
+++ sbootexp-security/src/main/java/io/github/zbrant/sbootexpsecurity/FooController.java
@@ -1,4 +1,20 @@
 package io.github.zbrant.sbootexpsecurity;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FooController {
+
+  @GetMapping("/public")
+  public ResponseEntity<String> publicRoute(){
+    return ResponseEntity.ok("Public route ok");
+  }
+
+  @GetMapping("/private")
+  public ResponseEntity<String> privateRoute(Authentication authentication ){
+    return ResponseEntity.ok("Private route ok, usuario conectado: " + authentication.getName());
+  }
 }
---
+++ sbootexp-security/src/test/java/io/github/zbrant/sbootexpsecurity/SbootexpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.sbootexpsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
