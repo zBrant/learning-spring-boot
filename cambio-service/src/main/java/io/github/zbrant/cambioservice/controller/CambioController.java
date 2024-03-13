bcd6ca843045293c753a9d8e5e1d411975d6951f
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-13 14:08:32 -0400

    new service

---
+++ cambio-service/src/main/java/io/github/zbrant/cambioservice/controller/CambioController.java
@@ -0,0 +1,27 @@
+package io.github.zbrant.cambioservice.controller;
+
+import io.github.zbrant.cambioservice.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping(value = "/{amount}/{from}/{to}")
+    public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
+                            @PathVariable("from") String from,
+                            @PathVariable("to") String to){
+        String port = environment.getProperty("local.server.port");
+        return new Cambio(1L, from, to, BigDecimal.ONE,BigDecimal.ONE, port);
+    }
+}
