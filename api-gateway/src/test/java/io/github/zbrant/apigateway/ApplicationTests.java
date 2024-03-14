3093eb6958c477577adc63f5e7dfbc53934d77c5
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-14 14:13:41 -0400

    gateway don't works

---
+++ api-gateway/src/test/java/io/github/zbrant/apigateway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
