6b2664ed462b5b7b81a177624e5da6c3ed4eb291
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-14 12:42:55 -0400

    service registry with Eureka

--- book-service/src/main/java/io/github/zbrant/bookservice/proxy/CambioProxy.java
+++ book-service/src/main/java/io/github/zbrant/bookservice/proxy/CambioProxy.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
-
 @FeignClient(name = "cambio-service", url = "localhost:8000")
 public interface CambioProxy {
 
