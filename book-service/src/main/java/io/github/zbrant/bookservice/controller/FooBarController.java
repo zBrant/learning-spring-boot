afc909b22632663bf13b5a52f0c3b031e2de5e50
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-15 10:26:41 -0400

    rate limiter

--- book-service/src/main/java/io/github/zbrant/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/io/github/zbrant/bookservice/controller/FooBarController.java
@@ -1,6 +1,8 @@
 package io.github.zbrant.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +18,15 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
     public String fooBar(){
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-//        return "Foo-Bar!!!";
-        return response.getBody();
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        return "Foo-Bar!!!";
+//        return response.getBody();
 
     }
 
