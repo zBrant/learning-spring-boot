be84b9bd9fd8d988981c97c8dd5502cb9c823ff6
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-25 08:23:50 -0400

    new project

---
+++ testes-unitarios/src/main/java/io/github/zbrant/testesunitarios/TestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.testesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesUnitariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestesUnitariosApplication.class, args);
+    }
+
+}
