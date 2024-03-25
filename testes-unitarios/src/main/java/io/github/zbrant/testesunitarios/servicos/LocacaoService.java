695722752f56a0a64c753cafd12393978e22ce84
Eduardo Brant <carlos23brant@hotmail.com>
2024-03-25 08:32:20 -0400

    new tests

---
+++ testes-unitarios/src/main/java/io/github/zbrant/testesunitarios/servicos/LocacaoService.java
@@ -0,0 +1,50 @@
+package io.github.zbrant.testesunitarios.servicos;
+
+import io.github.zbrant.testesunitarios.entidades.Filme;
+import io.github.zbrant.testesunitarios.entidades.Locacao;
+import io.github.zbrant.testesunitarios.entidades.Usuario;
+import io.github.zbrant.testesunitarios.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static io.github.zbrant.testesunitarios.utils.DataUtils.adicionarDias;
+import static io.github.zbrant.testesunitarios.utils.DataUtils.isMesmaData;
+
+public class LocacaoService {
+
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+
+    @Test
+    public void teste() {
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //acao
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificacao
+        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
