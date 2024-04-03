eb9c35b4a988cab9296385d21dbeb0b636bc779c
Eduardo Brant <carlos23brant@hotmail.com>
2024-04-03 10:52:24 -0400

    hands on bro

---
+++ learning-docker/node-mongo-compose/backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+
+//database 
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
+
+//teste 
+server.get('/', (req, res, next) => res.send('Backend'))
+
+// Start Server
+server.listen(3000)
