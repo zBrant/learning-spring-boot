d6253f6c2b1391d97b31a1c03c609d81adf1dcc3
Eduardo Brant <carlos23brant@hotmail.com>
2024-04-26 10:46:03 -0400

    Spring boot and Vuejs projects created from they templates

---
+++ project/project-api/src/main/java/io/github/zbrant/projectapi/ProjectApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.projectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiApplication.class, args);
+	}
+
+}
---
+++ project/project-api/src/test/java/io/github/zbrant/projectapi/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
