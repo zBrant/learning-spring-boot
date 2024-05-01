43b190ce2d4c981619e22f10359a4ccdd7fa5cad
EduardoBrant <carlos23brant@hotmail.com>
2024-05-01 17:52:11 -0400

    new project

---
+++ some-list/some-list-api/src/main/java/io/github/zbrant/somelistapi/SomeListApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.somelistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeListApiApplication.class, args);
+    }
+
+}
---
+++ some-list/some-list-api/src/test/java/io/github/zbrant/somelistapi/SomeListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.somelistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
